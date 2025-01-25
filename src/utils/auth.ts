ff90bbe57d6769b77936d251102e8a9779bbfaef
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-25 17:48:03 +0500

    Auth Api

---
+++ src/utils/auth.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+const API_BASE_URL = "http://localhost:8000/api/v1"
+
+export const signUp = async (userData: FormData) => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/signup`, userData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        })
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const login = async (email: string, password: string) => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/login`, { email, password })
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
